5e3fe66e9375766649206f478089ed93047be98e
streambinder <posta@davidepucci.it>
2020-04-13 23:38:43 +0200

    zeus: minify: exclude images optimization

--- zeus/scripts/minify.sh
+++ zeus/scripts/minify.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 find public -type f | while read -r fname; do
+
+    fname_base="$(basename "${fname}")"
+
     case "${fname##*.}" in
         html|css|js|json|svg|xml)
-            minify "${fname}" -o "/tmp/${fname}" && \
-                mv -f "/tmp/${fname}" "${fname}"
+            minify "${fname}" -o "/tmp/${fname_base}" && \
+                mv -f "/tmp/${fname_base}" "${fname}"
             ;;
-        png)
-            optipng -strip all -silent -o7 "${fname}" -out "/tmp/${fname}" && \
-                mv -f "/tmp/${fname}" "${fname}"
-            ;;
-        jpg|jpeg) \
-            jpegoptim -osqf "${fname}" -d /tmp && \
-                mv -f "/tmp/${fname}" "${fname}"
-            ;;
-        *)
-            ;;
+        # png)
+        #     optipng -strip all -silent -o7 "${fname}" -out "/tmp/${fname_base}" && \
+        #         mv -f "/tmp/${fname_base}" "${fname}"
+        #     ;;
+        # jpg|jpeg) \
+        #     jpegoptim -osqf "${fname}" -d /tmp && \
+        #         mv -f "/tmp/${fname_base}" "${fname}"
+        #     ;;
+        # *)
+        #     ;;
     esac
 done
