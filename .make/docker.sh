b5cf967c84cf1302f35b6c5064f40bb99d27ae3c
streambinder <posta@davidepucci.it>
2020-11-17 14:00:13 +0100

    make: docker: release to GitHub Container Registry

--- .make/docker.sh
+++ .make/docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker build . -t streambinder/davidepucci:latest
-docker push streambinder/davidepucci:latest
+echo "${GITHUB_ACTIONS_DAVIDEPUCCI}" | docker login ghcr.io -u streambinder --password-stdin
+docker build -t ghcr.io/streambinder/streambinder:latest .
+docker push ghcr.io/streambinder/streambinder:latest
