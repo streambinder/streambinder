a7e126e38168bfd96419fb76883ca6612a945da2
streambinder <posta@davidepucci.it>
2026-06-11 17:10:28 +0200

    fix(site): address crawler audit findings
    Add CSP, robots.txt, correct heading structure, attribute quoting,
    aria labels, MIME types, and cache rules.

--- .make/minify.sh
+++ .make/minify.sh
@@ -3,19 +3,13 @@
 find "${BUILD_DIR}" -type f | while read -r fname; do
 	fname_base="$(basename "${fname}")"
 	case "${fname##*.}" in
-	html | css | js | json | svg | xml)
+	html)
+		minify --html-keep-quotes "${fname}" -o "/tmp/${fname_base}" &&
+			mv -f "/tmp/${fname_base}" "${fname}"
+		;;
+	css | js | json | svg | xml)
 		minify "${fname}" -o "/tmp/${fname_base}" &&
 			mv -f "/tmp/${fname_base}" "${fname}"
 		;;
-		# png)
-		#     optipng -strip all -silent -o7 "${fname}" -out "/tmp/${fname_base}" && \
-		#         mv -f "/tmp/${fname_base}" "${fname}"
-		#     ;;
-		# jpg|jpeg) \
-		#     jpegoptim -osqf "${fname}" -d /tmp && \
-		#         mv -f "/tmp/${fname_base}" "${fname}"
-		#     ;;
-		# *)
-		#     ;;
 	esac
 done
